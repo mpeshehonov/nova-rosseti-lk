1c6b3f0c1b9f3ef6cc33f707cc5a96124897fe0c
Peshekhonov Maksim <peshekhonov.m@citilink.ru>
2021-12-04 03:43:55 +0300

    исправлены ошибки билда и добавлена фавиконка

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = ({ className }: { className: string }) => (
-  <img className={className} src="/logo.png" alt="Россети" />
+  <img className={className} src="./logo.png" alt="Россети" />
 );
 
 export default Logo;
